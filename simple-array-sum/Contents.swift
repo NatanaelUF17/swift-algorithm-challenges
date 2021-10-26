859b6445235c265d602fb0149f1ef9b987cca16f
Natanael <NatanaelUF17@gmail.com>
2021-10-26 12:32:11 -0400

    Uploading simple array sum challenge

---
+++ simple-array-sum/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+func simpleArraySum(ar: [Int]) -> Int {
+    var result: Int = 0
+    
+    for x in 0..<ar.count {
+        result += ar[x]
+    }
+    
+    return result
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let arCount = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let ar: [Int] = arTemp.split(separator: " ").map {
+    if let arItem = Int($0) {
+        return arItem
+    } else { fatalError("Bad input") }
+}
+
+guard ar.count == arCount else { fatalError("Bad input") }
+
+let result = simpleArraySum(ar: ar)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
